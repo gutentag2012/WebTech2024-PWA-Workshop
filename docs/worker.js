94217c7c71abe3f29d737d3b78a77729a2d92f99
gutentag2012 <joshua.gawenda+github@gmail.com>
2024-07-09 14:21:11 +0200

    fix: remove window

--- docs/worker.js
+++ docs/worker.js
@@ -2,10 +2,8 @@ console.log("Loaded service worker");
 
 self.addEventListener('install', (event) => {
     console.log("Service worker installed");
-    window.alert("Service worker installed");
 });
 
 self.addEventListener('activate', (event) => {
     console.log("Service worker activated");
-    window.alert("Service worker activated");
 });
